f720f7a6e9709d2da3255c2b7a914c415783911e
Leandro <leandro.schaab@hotmail.com>
2024-04-07 20:05:43 -0300

    new changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [icon(), image()]
 });
